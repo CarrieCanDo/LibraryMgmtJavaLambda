c49f36c33fd8b3afc7e003865c8d9105041bb447
CarrieCanDo <carrie.a.wright@outlook.com>
2024-08-05 15:13:11 -0400

    Committing files

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -5,6 +5,55 @@ public class Book {
     private String author;
     private int publicationYear;
     private int pages;
-}
+
+
+    public Book(String title, String author, int publicationYear, int pages) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title= " + title +
+                ", author= " + author +
+                ", publicationYear= " + publicationYear +
+                ", pages= " + pages +
+                "}";
+    }
 
 }
--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,4 +1,41 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    //Method to add a book to library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    //Method to remove a book from the library by title
+    public void removeBook(String title) {
+        books.removeIf(book -> book.getTitle().equals(title));
+
+    }
+
+    //Method to find all books published in a specific year
+    public List<Book> findBooksByYear(int year) {
+        return books.stream()
+                .filter(book -> book.getPublicationYear() == year)
+                .collect(Collectors.toList());
+        //method to find books by author
+
+        public List<Book> findBooksByAuthor (String author){
+            return books.stream()
+                    .filter(book -> book.getAuthor().equais(author))
+                    .collect(Collectors.toList());
+
+
+        }
+    }
+
 }
